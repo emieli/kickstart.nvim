bfee3c05b00dd24c42f4628eaa2ae4c13f2c73cc
emieli <emieli>
2025-04-09 21:28:30 +0200

    Add golang templ LSP

--- init.lua
+++ init.lua
@@ -687,6 +687,7 @@ require('lazy').setup({
       local servers = {
         -- clangd = {},
         gopls = {},
+        templ = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
