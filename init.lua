bc173c339f2ff595f64dced0ff1d611040b751b0
emileli <emil.eliasson@tele2.com>
2025-04-13 22:19:26 +0200

    Add Ansible LSP

--- init.lua
+++ init.lua
@@ -241,6 +241,9 @@ require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
 
+  -- Enhanced ansible-language-server mason plugin
+  'mfussenegger/nvim-ansible',
+
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
   -- keys can be used to configure plugin behavior/loading/etc.
@@ -688,6 +691,7 @@ require('lazy').setup({
         -- clangd = {},
         gopls = {},
         templ = {},
+        ansiblels = {}, -- https://ansible-language-server.readthedocs.io/en/latest/
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
